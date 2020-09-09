885113b7af6ec29d2ac0e521ee7d0dca72c86cc4
aure <aurelientav@gmail.com>
2020-09-09 11:34:25 +0200

    LA MAP MARCHE ( et la géolocalisation!!!!!!!!!!!)

--- src/js/api_google.js
+++ src/js/api_google.js
@@ -1,8 +1,39 @@
-// Initialize and add the map
-function initMap() {
+var map, infoWindow;
+window.initMap = function() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
+    var uluru = {lat: 0, lng: 0};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 10, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+infoWindow = new google.maps.InfoWindow;
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+    }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+    });
+}
+else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+}
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }

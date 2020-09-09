60641482d787f5957629479a42ac3b2004406365
RedDansTaGueule <a.deyris85@gmail.com>
2020-09-09 17:32:07 +0200

    API SNCF

--- public/js/app.js
+++ public/js/app.js
@@ -18058,18 +18058,55 @@ module.exports = g;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var callBackGetSuccess = function callBackGetSuccess(data) {
-  console.log("donnees api", data);
+var requestURL = 'https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:OCE:SA:87391003/departures?datetime=20200909T104704';
+var request = new XMLHttpRequest();
+var jsonObj2;
+request.addEventListener("progress", updateProgress, false);
+request.addEventListener("load", transferComplete, false);
+request.addEventListener("error", transferFailed, false);
+request.addEventListener("abort", transferCanceled, false);
+request.open('GET', requestURL);
+
+request.onload = function (e) {
+  console.log("cc");
+  var superHeroes = request.response;
+  populateHeader(superHeroes);
 };
 
-window.buttonAPI = function () {
-  var url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_tan-arrets-horaires-circuits&q=&lang=FR";
-  $.get(url, callBackGetSuccess).done(function () {//alert( "second success" );
-  }).fail(function () {
-    alert("error");
-  }).always(function () {//alert ( "finished" );
-  });
-};
+request.setRequestHeader("Authorization", "f30d872d-839e-4488-82eb-6e97db5e38f9");
+request.responseType = 'json';
+request.send();
+
+function populateHeader(jsonObj) {
+  console.log("yo");
+  jsonObj2 = jsonObj;
+  var myH1 = document.createElement('h1'); //myH1.textContent = jsonObj.departures.0.display_informations.direction;
+
+  for (var i = 0; i < jsonObj2.departures.length; i++) {
+    console.log(jsonObj2.departures[i].display_informations.direction);
+  }
+} // ...
+// progression des transferts depuis le serveur jusqu'au client (téléchargement)
+
+
+function updateProgress(oEvent) {
+  if (oEvent.lengthComputable) {
+    var percentComplete = oEvent.loaded / oEvent.total; // ...
+  } else {// Impossible de calculer la progression puisque la taille totale est inconnue
+    }
+}
+
+function transferComplete(evt) {
+  alert("Le transfert est terminé.");
+}
+
+function transferFailed(evt) {
+  alert("Une erreur est survenue pendant le transfert du fichier.");
+}
+
+function transferCanceled(evt) {
+  alert("Le transfert a été annulé par l'utilisateur.");
+}
 
 /***/ }),
 
--- src/js/api-tan.js
+++ src/js/api-tan.js
@@ -1,17 +1,55 @@
-let callBackGetSuccess = function(data) {
-    console.log("donnees api", data)
+let requestURL = 'https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:OCE:SA:87391003/departures?datetime=20200909T104704';
+let request = new XMLHttpRequest();
+var jsonObj2;
+
+request.addEventListener("progress", updateProgress, false);
+request.addEventListener("load", transferComplete, false);
+request.addEventListener("error", transferFailed, false);
+request.addEventListener("abort", transferCanceled, false);
+
+request.open('GET', requestURL);
+
+request.onload = function(e) {
+    console.log("cc");
+    let superHeroes = request.response;
+    populateHeader(superHeroes);
+}
+request.setRequestHeader("Authorization","f30d872d-839e-4488-82eb-6e97db5e38f9");
+request.responseType = 'json';
+request.send();
+
+function populateHeader(jsonObj) {
+    console.log("yo");
+    jsonObj2 = jsonObj;
+    let myH1 = document.createElement('h1');
+    //myH1.textContent = jsonObj.departures.0.display_informations.direction;
+    for(let i=0; i<jsonObj2.departures.length; i++) {
+        console.log(jsonObj2.departures[i].display_informations.direction);
+    }
 }
 
-window.buttonAPI = function() {
-    let url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_tan-arrets-horaires-circuits&q=&lang=FR";
 
-    $.get(url, callBackGetSuccess).done(function() {
-        //alert( "second success" );
-    })
-        .fail(function() {
-            alert( "error" );
-        })
-        .always(function() {
-            //alert ( "finished" );
-        });
+// ...
+
+// progression des transferts depuis le serveur jusqu'au client (téléchargement)
+function updateProgress (oEvent) {
+    if (oEvent.lengthComputable) {
+        var percentComplete = oEvent.loaded / oEvent.total;
+        // ...
+    } else {
+        // Impossible de calculer la progression puisque la taille totale est inconnue
+    }
 }
+
+function transferComplete(evt) {
+    alert("Le transfert est terminé.");
+}
+
+function transferFailed(evt) {
+    alert("Une erreur est survenue pendant le transfert du fichier.");
+}
+
+function transferCanceled(evt) {
+    alert("Le transfert a été annulé par l'utilisateur.");
+}
+

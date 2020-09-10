d19514c85718a46fa386e534e97fd1aa30fdb098
RedDansTaGueule <a.deyris85@gmail.com>
2020-09-10 11:04:26 +0200

    Nik la TAN

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let express = require("express");
 let path = require("path");
 let routes = require("./routes");
+let cors = require('cors');
 let app = express();
+app.use(cors())
 
 app.set("port", process.env.PORT || 3500);
 app.set("views", path.join(__dirname, "views"));
@@ -9,6 +11,7 @@ app.set("view engine", "ejs");
 
 app.use(routes);
 app.use(express.static('public'));
+
 
 app.listen(app.get("port"), function(){
     console.log("Server started on port " + app.get("port"))
--- src/js/api-tan.js
+++ src/js/api-tan.js
@@ -1,55 +1,27 @@
-let requestURL = 'https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:OCE:SA:87391003/departures?datetime=20200909T104704';
-let request = new XMLHttpRequest();
-var jsonObj2;
+let loading = true;
+let errored = false;
+let errors = null;
+let data = null;
+let requestURL = 'http://open.tan.fr/ewp/arrets.json/47,21661/-1,556754';
 
-request.addEventListener("progress", updateProgress, false);
-request.addEventListener("load", transferComplete, false);
-request.addEventListener("error", transferFailed, false);
-request.addEventListener("abort", transferCanceled, false);
+console.log('DEBUT TAN')
 
-request.open('GET', requestURL);
-
-request.onload = function(e) {
-    console.log("cc");
-    let superHeroes = request.response;
-    populateHeader(superHeroes);
-}
-request.setRequestHeader("Authorization","f30d872d-839e-4488-82eb-6e97db5e38f9");
-request.responseType = 'json';
-request.send();
-
-function populateHeader(jsonObj) {
-    console.log("yo");
-    jsonObj2 = jsonObj;
-    let myH1 = document.createElement('h1');
-    //myH1.textContent = jsonObj.departures.0.display_informations.direction;
-    for(let i=0; i<jsonObj2.departures.length; i++) {
-        console.log(jsonObj2.departures[i].display_informations.direction);
-    }
-}
-
-
-// ...
-
-// progression des transferts depuis le serveur jusqu'au client (téléchargement)
-function updateProgress (oEvent) {
-    if (oEvent.lengthComputable) {
-        var percentComplete = oEvent.loaded / oEvent.total;
-        // ...
-    } else {
-        // Impossible de calculer la progression puisque la taille totale est inconnue
-    }
-}
-
-function transferComplete(evt) {
-    alert("Le transfert est terminé.");
-}
-
-function transferFailed(evt) {
-    alert("Une erreur est survenue pendant le transfert du fichier.");
-}
-
-function transferCanceled(evt) {
-    alert("Le transfert a été annulé par l'utilisateur.");
-}
-
+axios.get(requestURL, {
+    'Access-Control-Allow-Origin': '80.10.235.197',
+})
+    .then(response => {
+        console.log('RESPONSE TAN')
+        console.log(response);
+        data = response.data;
+    })
+    .catch(error => {
+        console.log('ERROR TAN')
+        console.log(error);
+        errors = error;
+        errored = true;
+    })
+    .finally(() => {
+        loading = false;
+        console.log('DATA TAN')
+        console.log(data);
+    });
